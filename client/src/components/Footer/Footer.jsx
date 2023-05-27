46c91e171eeb5da549e2633c94c892445edbf7c3
Sanket Borade <boradesanket13@gmail.com>
2023-05-27 22:34:23 +0530

    backend

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div className="col">
+                    <div className="title">About</div>
+                    <div className="text">
+                    Whether you're looking for noise-cancelling earphones for a peaceful commute or waterproof earbuds for your workout, we've got you covered.
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Contact</div>
+                    <div className="c-item">
+                        <FaLocationArrow />
+                        <div className="text">
+                            College Road, Nashik 422 005
+                        </div>
+                    </div>
+                    <div className="c-item">
+                        <FaMobileAlt />
+                        <div className="text">Phone: 9370450837</div>
+                    </div>
+                    <div className="c-item">
+                        <FaEnvelope />
+                        <div className="text">Email: store@aura.com</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Categories</div>
+                    <span className="text">Headphones</span>
+                    <span className="text">Smart Watches</span>
+                    <span className="text">Bluetooth Speakers</span>
+                    <span className="text">Wireless Earbuds</span>
+                    <span className="text">Home Theatre</span>
+                    <span className="text">Projectors</span>
+                </div>
+                <div className="col">
+                    <div className="title">Pages</div>
+                    <span className="text">Home</span>
+                    <span className="text">About</span>
+                    <span className="text">Privacy Policy</span>
+                    <span className="text">Returns</span>
+                    <span className="text">Terms & Conditions</span>
+                    <span className="text">Contact Us</span>
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <div className="bottom-bar-content">
+                    <span className="text">
+                        AURA
+                    </span>
+                    <img src={Payment} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ client/src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>AURA</h1>
+                    <p>
+                    Whether you're looking for noise-cancelling earphones for a peaceful commute or waterproof earbuds for your workout, we've got you covered.
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
